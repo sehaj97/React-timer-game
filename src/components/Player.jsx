821b9dfe3bd0945080b7eec9939754fc9762aeec
Sehajpreet Singh <sehajmagan@gmail.com>
2024-03-28 23:38:00 -0400

    updated usecase for useref hook

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,25 @@
+import { useRef, useState } from "react";
+
 export default function Player() {
+  const player = useRef()
+  const [playerName, setPlayerName] = useState(null)
+  // const [isSubmitted, setIsSubmitted] = useState(false)
+  // function handleChange(event) {
+  //   setIsSubmitted(false)
+  //   setPlayerName(event.target.value)
+  // }
+  function handleClick() {
+    // setIsSubmitted(true)
+    setPlayerName(player.current.value)
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      {/* <h2>Welcome {(isSubmitted) ? playerName : "unknown entity"}</h2> */}
+      <h2>Welcome {playerName ?? "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        {/* <input ref={player} type="text" onChange={handleChange} value={playerName} /> */}
+        <input ref={player} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
